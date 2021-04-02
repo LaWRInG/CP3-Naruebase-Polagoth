1154d70dc08cf078262d02d5704f7a227260a672
Jame Naruebase <naruebaselearn@gmail.com>
2021-04-02 11:41:31 +0700

    Ver.1
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Naruebase_P.py
@@ -0,0 +1,9 @@
+eng = float(80)
+busi = float(80)
+inToCom = float(80)
+ComPro = float(80)
+
+print("Foundation English : ", eng)
+print("General Business : ", busi)
+print("Introduction to Computer Systems : ", inToCom)
+print("Computer Programming : ", ComPro)
