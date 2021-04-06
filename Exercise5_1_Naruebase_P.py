688326e58c9c628fbca0e46e8211d36c8e9f12ec
Jame Naruebase <naruebaselearn@gmail.com>
2021-04-06 11:57:21 +0700

    exercise 5
    1-2 input

---
+++ Exercise5_1_Naruebase_P.py
@@ -0,0 +1,12 @@
+x = int(input("Please input the first integer number : "))
+y = int(input("Please input the second integer number : "))
+
+result_1 = x + y
+result_2 = x - y
+result_3 = x * y
+result_4 = x / y
+
+print(x, '+', y, "=", result_1)
+print(x, "-", y, '=', result_2)
+print(x, "*", y, "=", result_3)
+print(x, "/", y, "=", result_4)
---
+++ Exercise5_2_Naruebase_P.py
@@ -0,0 +1,6 @@
+s = float(input("Please input distance (Km) : "))
+t = float(input("Please input time (Hr) : "))
+
+v = s / t
+
+print("the speed is", v, "Km/Hr")
